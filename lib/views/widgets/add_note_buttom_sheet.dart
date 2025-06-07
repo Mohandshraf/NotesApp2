e8bf580c6a0bbbce1b94757aca418a2bd6bbfb85
Mohandshraf <information123890@gmail.com>
2025-06-07 20:00:57 +0300

    Refactor CustomTextField to accept hint and maxLines parameters; update AddNoteBottomSheet to use new CustomTextField implementation

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -8,7 +8,14 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 32),
-      child: Column(children: [SizedBox(height: 16), CustomTextField()]),
+      child: Column(
+        children: [
+          SizedBox(height: 16),
+          CustomTextField(hint: "Title"),
+          SizedBox(height: 25),
+          CustomTextField(hint: "Content", maxLines: 5),
+        ],
+      ),
     );
   }
 }
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  final String? hint;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: "Title",
+        hintText: hint,
         hintStyle: TextStyle(color: Colors.white),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
