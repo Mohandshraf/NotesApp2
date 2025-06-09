1ad4801bfa1a2c2553aa8f2572f7fdab2320a976
Mohandshraf <information123890@gmail.com>
2025-06-09 18:07:52 +0300

    Integrate BlocConsumer in AddNoteButtomSheet for state management and add modal progress indicator

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notesapp/cubits/add_notes_cubit.dart';
 import 'package:notesapp/views/widgets/add_note_form.dart';
-import 'package:notesapp/views/widgets/custom_buttom.dart';
-import 'package:notesapp/views/widgets/custom_text_field.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
@@ -10,7 +11,25 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 32),
-      child: SingleChildScrollView(child: AddNoteForm()),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNotesState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print("state.message");
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+
+              child: AddNoteForm(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
